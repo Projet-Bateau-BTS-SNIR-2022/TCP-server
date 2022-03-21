be0ec901634c2c73f0d506349879d6416913ad7d
Flotroniks <47397500+Flotroniks@users.noreply.github.com>
2022-03-21 11:47:22 +0100

    Add files via upload

---
+++ TCP-Serveur.h
@@ -0,0 +1,11 @@
+#ifndef SERVEURTCP_PROJETBTS_TCP_SERVEUR_H
+#define SERVEURTCP_PROJETBTS_TCP_SERVEUR_H
+
+#endif //SERVEURTCP_PROJETBTS_TCP_SERVEUR_H
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+int sendData(string message);
